0d75d3fa72981ccc651c5503376cb3cde3391c20
pavankoushik22 <pavankoushik22@gmail.com>
2017-06-26 16:33:51 +0530

    Setup email functionality for sign-up

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+	:address => 'smtp.sendgrid.net',
+	:port => '587',
+	:authenticate => :plain,
+	:user_name => ENV['SENDGRID-USERNAME'],
+	:password => ENV['SENDGRID-PASSWORD'],
+	:domain => 'heroku.com',
+	:enable_starttls_auto => true
+}
