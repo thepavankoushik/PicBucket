863dd5a9fe39d2adaf031f26cd50bbecd175b014
pavankoushik22 <pavankoushik22@gmail.com>
2017-06-27 15:21:06 +0530

    delivery_method addition

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,13 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
+ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
+	:user_name => "app71462278@heroku.com",
+	:password => "nvchhesu4284",
+	:domain => 'heroku.com',
 	:address => 'smtp.sendgrid.net',
 	:port => '587',
 	:authentication => :plain,
-	:user_name => ENV['SENDGRID-USERNAME'],
-	:password => ENV['SENDGRID-PASSWORD'],
-	:domain => 'heroku.com',
 	:enable_starttls_auto => true
 }
