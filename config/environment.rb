22d6d2f781dde30bcfd92407fc516f05114e061b
pavankoushik22 <pavankoushik22@gmail.com>
2017-06-28 18:13:37 +0530

    adding some extra salt

--- config/environment.rb
+++ config/environment.rb
@@ -6,8 +6,8 @@ Rails.application.initialize!
 
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-	:user_name => "app71462278@heroku.com",
-	:password => "nvchhesu4284",
+	:user_name => ENV['SENDGRID_USERNAME'],
+	:password => ENV['SENDGRID_PASSWORD'],
 	:domain => 'heroku.com',
 	:address => 'smtp.sendgrid.net',
 	:port => '587',
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,10 +2,10 @@ if Rails.env.production?
 	CarrierWave.configure do |config|
 		config.fog_credentials = {
 			:provider => 'AWS',
-			:aws_access_key_id => 'AKIAJOKHKFP6IQCEVZOQ',
-			:aws_secret_access_key => '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'
+			:aws_access_key_id => ENV['S3_ACCESS_KEY'],
+			:aws_secret_access_key => ENV['S3_SECRET_KEY'] 
 		}
-		config.fog_directory = 'pk-photo-app'
+		config.fog_directory = ENV['S3_BUCKET']
 	end
 
 end
