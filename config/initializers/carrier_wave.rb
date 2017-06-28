5ae1d1b6acea2a73c83e484f921d51f01b45b810
pavankoushik22 <pavankoushik22@gmail.com>
2017-06-28 16:17:08 +0530

    add prod image uploader

---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,11 @@
+if Rails.env.production?
+	CarrierWave.configure do |config|
+		config.fog_credentials = {
+			:provider => 'AWS'
+			:aws_access_key_id => 'AKIAJOKHKFP6IQCEVZOQ',
+			:aws_secret_access_key => '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'
+		}
+		config.fog_directory = 'pk-photo-app'
+	end
+
+end
